ddf3f6385c97d90c618de458e897ac46500aa213
Jb00911 <130429930+Jb00911@users.noreply.github.com>
2026-02-26 05:17:41 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,7 +31,6 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"]
   }
 })
-});
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Atlas connected"))
   .catch((err) => console.log(err))
